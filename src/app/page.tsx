6c051527e3f8f1c34acb92ae57ed1b8c1356aac9
ly <ly_felix@foxmail.com>
2024-03-28 18:59:14 +0800

    update several features, fixed prisma vercel bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl">Home</h1>;
-      <Link className={buttonVariants()} href='/admin'> open my dashboard </Link>
+      <h1 className="text-4xl">Home</h1>
+      <Link className={buttonVariants()} href="/admin">
+        {" "}
+        open my dashboard{" "}
+      </Link>
     </div>
   );
 }
